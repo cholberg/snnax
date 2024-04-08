4b4c29d1425b162734e4a865cc33a4c5d29e254f
cholberg <cholberg24@gmail.com>
2024-04-08 15:04:44 +0100

    Updated `expected_signature_loss`
    The previous version of the loss was not very trainable. Since some coordinates of the signature depend on the number of spikes, when comparing spike trains with fundamentally different spike counts, the gradient would end up being in the wrong direction. (The direction in which to step to increase the number of spikes also decreases the spike times of the present spikes)
    
    Now, for two samples of the same size, spike trains are paired up arbitrarily (rather arbitrarily, first sample of the first batch with first sample of the second batch, etc.), and the number of spikes is truncated to the lowest among the pair.

--- event_rde/losses.py
+++ event_rde/losses.py
@@ -14,6 +14,11 @@ def expected_signature(y: Float[Array, ""], depth: int) -> Array:
 def expected_signature_loss(
     y_1: Float[Array, "... dim"], y_2: Float[Array, "... dim"], depth: int
 ) -> Real:
-    sig_1 = expected_signature(y_1, depth)
-    sig_2 = expected_signature(y_2, depth)
+    spike_counts_1 = jnp.max(y_1[:, :, 1:], axis=1)
+    spike_counts_2 = jnp.max(y_2[:, :, 1:], axis=1)
+    spike_counts = jnp.minimum(spike_counts_1, spike_counts_2)
+    y_1_trunc = y_1.at[:, :, 1:].set(jax.vmap(jnp.minimum)(y_1[:, :, 1:], spike_counts))
+    y_2_trunc = y_2.at[:, :, 1:].set(jax.vmap(jnp.minimum)(y_2[:, :, 1:], spike_counts))
+    sig_1 = expected_signature(y_1_trunc, depth)
+    sig_2 = expected_signature(y_2_trunc, depth)
     return jnp.mean((sig_1 - sig_2) ** 2)
