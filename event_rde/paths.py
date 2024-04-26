367555b5c5ac027f80c8ee699b604558a78386ab
cholberg <cholberg24@gmail.com>
2024-04-26 16:50:27 +0100

    Added differentiable Brownian path to be used by diffrax and a convenience function for plotting paths

--- event_rde/paths.py
+++ event_rde/paths.py
@@ -1,11 +1,30 @@
-from typing import Optional
+from typing import Literal, Optional, Tuple, Union
 
+import equinox as eqx
+import equinox.internal as eqxi
 import jax
 import jax.numpy as jnp
 import jax.tree_util as jtu
-from diffrax import AbstractPath
+from diffrax import AbstractPath, BrownianIncrement, SpaceTimeLevyArea, VirtualBrownianTree
+from diffrax._brownian.tree import _levy_diff, _make_levy_val, levy_tree_transpose, linear_rescale
 from diffrax._custom_types import RealScalarLike
-from jaxtyping import Array, Float, Real
+from jaxtyping import Array, Float, PRNGKeyArray, PyTree, Real
+from typing_extensions import TypeAlias
+
+_Spline: TypeAlias = Literal["sqrt", "quad", "zero"]
+
+
+def plottable_path(
+    ts: Real[Array, "spikes times"], ys: Float[Array, "spikes neurons times 3"]
+) -> Tuple[Real[Array, " times"], Float[Array, "neurons times 3"]]:
+    _, neurons, times, _ = ys.shape
+    idx = jnp.max(jnp.cumsum(ys[:, :, :, 2] > 0, axis=2), axis=1) < 1
+    idx_y = jnp.tile(idx[:, None, :], (1, neurons, 1))
+    ys_flat = ys[idx_y].reshape((neurons, -1, 3))
+    ts_flat = ts[idx]
+    ts_out = jnp.linspace(ts_flat[0], ts_flat[-1], times)
+    ys_out = ys_flat[:, jnp.searchsorted(ts_flat, ts_out)]
+    return ts_out, ys_out
 
 
 def interleave(arr1: Array, arr2: Array) -> Array:
@@ -46,15 +65,15 @@ class SpikeTrain(AbstractPath):
     num_neurons: int
 
     def __init__(self, t0, t1, spike_times, spike_mask):
+        max_spikes, num_neurons = spike_mask.shape
+        self.num_neurons = num_neurons
         self.t0 = t0
         self.t1 = t1
         self.num_spikes = spike_times.shape[0]
         self.spike_times = jnp.insert(spike_times, 0, t0)
-        self.spike_cumsum = jnp.array(
-            jtu.tree_map(lambda x: jnp.cumsum(jnp.insert(x, 0, jnp.array(False))), spike_mask),
-            dtype=float,
+        self.spike_cumsum = jnp.cumsum(
+            jnp.insert(spike_mask, 0, jnp.full_like(spike_mask[0], False), axis=0), axis=0
         )
-        self.num_neurons = self.spike_cumsum.shape[0]
 
     def evaluate(self, t0: Real, t1: Optional[Real] = None, left: Optional[bool] = True) -> Array:
         del left
@@ -62,5 +81,49 @@ class SpikeTrain(AbstractPath):
             return self.evaluate(t1 - t0)
         idx = jnp.searchsorted(self.spike_times, t0)
         idx = jnp.where(idx > 0, idx - 1, idx)
-        out = jax.lax.dynamic_slice(self.spike_cumsum, (0, idx), (self.num_neurons, 1))[:, 0]
+        out = jax.lax.dynamic_slice(self.spike_cumsum, (idx, 0), (self.num_neurons, 1))[:, 0]
         return out
+
+
+# A version of VirtualBrownianTree that will not throw an error when differentiated
+class BrownianPath(VirtualBrownianTree):
+    @eqxi.doc_remove_args("_spline")
+    def __init__(
+        self,
+        t0: RealScalarLike,
+        t1: RealScalarLike,
+        tol: RealScalarLike,
+        shape: Union[tuple[int, ...], PyTree[jax.ShapeDtypeStruct]],
+        key: PRNGKeyArray,
+        levy_area: type[Union[BrownianIncrement, SpaceTimeLevyArea]] = BrownianIncrement,
+        _spline: _Spline = "sqrt",
+    ):
+        super().__init__(t0, t1, tol, shape, key, levy_area, _spline)
+
+    @eqx.filter_jit
+    def evaluate(
+        self,
+        t0: RealScalarLike,
+        t1: Optional[RealScalarLike] = None,
+        left: bool = True,
+        use_levy: bool = False,
+    ) -> Union[PyTree[Array], BrownianIncrement, SpaceTimeLevyArea]:
+        t0 = jax.lax.stop_gradient(t0)
+        # map the interval [self.t0, self.t1] onto [0,1]
+        t0 = linear_rescale(self.t0, t0, self.t1)
+        levy_0 = self._evaluate(t0)
+        if t1 is None:
+            levy_out = levy_0
+            levy_out = jtu.tree_map(_make_levy_val, self.shape, levy_out)
+        else:
+            t1 = jax.lax.stop_gradient(t1)
+            # map the interval [self.t0, self.t1] onto [0,1]
+            t1 = linear_rescale(self.t0, t1, self.t1)
+            levy_1 = self._evaluate(t1)
+            levy_out = jtu.tree_map(_levy_diff, self.shape, levy_0, levy_1)
+
+        levy_out = levy_tree_transpose(self.shape, levy_out)
+        # now map [0,1] back onto [self.t0, self.t1]
+        levy_out = self._denormalise_bm_inc(levy_out)
+        assert isinstance(levy_out, (BrownianIncrement, SpaceTimeLevyArea))
+        return levy_out if use_levy else levy_out.W
